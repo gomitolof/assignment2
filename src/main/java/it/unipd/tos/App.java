e12a526f9ed0ef5b8434075cb22e5eb61857a91c
Francesco <fifreda@hotmail.it>
2019-12-06 17:05:35 +0100

    configurazione maven, analisi statica e travis

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Francesco Freda 1143643
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
